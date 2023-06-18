41aa815e0d4cf4b9915535155b092a5edbb0fbff
Luismi21 <lrios390@accitesz.com>
2023-06-17 21:53:07 -0600

    ultimo comit de login en angular

---
+++ src/app/inicio-doctor/inicio-doctor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio-doctor',
+  templateUrl: './inicio-doctor.component.html',
+  styleUrls: ['./inicio-doctor.component.css']
+})
+export class InicioDoctorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
